997e32c4a3956181047a66f93a0c89826524cdf6
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-16 13:39:12 -0400

    Added new game set values to zero with For Loop.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let score = 0;
+let currentScoreStart = 0;
+
+const scoreReset = document.querySelectorAll(".score, .current-score");
+
+for (let i = 0; i < scoreReset.length; i++){
+    scoreReset[i].textContent = score;
+}
