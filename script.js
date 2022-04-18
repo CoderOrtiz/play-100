bc56c9f50ab153276b574728371dd5d09389fb35
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-17 20:52:05 -0400

    Added a hidden class and current score now adds

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 let score = 0;
-let currentScoreStart = 0;
+let currentScore = 0;
 let roll = 6;
-
+document.querySelector("img").classList.add("hidden");
 const scoreReset = document.querySelectorAll(".score, .current-score");
+
 
 for (let i = 0; i < scoreReset.length; i++){
     scoreReset[i].textContent = score;
@@ -17,8 +18,16 @@ document.querySelector(".btn--roll").addEventListener("click", rollDice);
 // document.querySelector(".btn--new").addEventListener("click", newgame);
 
 function rollDice(){
+  document.querySelector("img").classList.remove("hidden");
   roll = Math.floor(Math.random() * 6) + 1;
   const diceRolled = `./images/dice-${roll}.png`;
   document.querySelector("img").setAttribute("src", diceRolled);
+  if (roll !== 1) {
+    // Same as currentScore = currentScore + roll
+    currentScore += roll;
+
+   document.querySelector("#current--0").textContent = currentScore;
+
+}
 }
 
