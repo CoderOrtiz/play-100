f72d50ec2829943573e0303a32240c4e6c9b6ff5
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-19 14:07:27 -0400

    Added a new class for when a player wins the background turns green

--- script.js
+++ script.js
@@ -2,10 +2,28 @@
 
 let score = 0;
 let currentScore = 0;
-let roll = 6;
 let activePlayer = 0;
+let roll = 0
+let player1sTurn = document.querySelector(".player--0");
+let player2sTurn = document.querySelector(".player--1");
+let savedScores = [0, 0];
 document.querySelector("img").classList.add("hidden");
-const scoreReset = document.querySelectorAll(".score, .current-score");
+let scoreReset = document.querySelectorAll(".current-score, .score");
+const hold = document.querySelector(".btn--hold");
+const youWonBackground = document.querySelector("body");
+
+function switchPlayer (){
+// If Player 1 Rolls a 1, then Player's 1 Score gets Reset to 0
+document.querySelector(`#current--${activePlayer}`).textContent = 0;
+// Resets the Current Score back to 0, Otherwise Player's 1 Score Would Automaticly Transfer to Player 2
+currentScore = 0;
+// Switches the Active Player's Value from 0 to 1, which causes the Switch from Player 1 to Player 2
+activePlayer = activePlayer === 0 ? 1 : 0;
+// Toggles the "player--active" Class Between Player 1 & 2
+player1sTurn.classList.toggle("player--active");
+player2sTurn.classList.toggle("player--active");
+}
+                                                      
 
 
 for (let i = 0; i < scoreReset.length; i++){
@@ -13,6 +31,7 @@ for (let i = 0; i < scoreReset.length; i++){
 }
 
 document.querySelector(".btn--roll").addEventListener("click", rollDice);
+document.querySelector(".btn--hold").addEventListener("click", hold);
 
 // document.querySelector(".btn--hold").addEventListener("click", holdNumber);
 
@@ -23,19 +42,32 @@ function rollDice(){
   roll = Math.floor(Math.random() * 6) + 1;
   const diceRolled = `./images/dice-${roll}.png`;
   document.querySelector("img").setAttribute("src", diceRolled);
+  
   if (roll !== 1) {
     // Same as currentScore = currentScore + roll
     currentScore += roll;
-
+    
    document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+
+   
   }else {
-    // If Player 1 Rolls a 1, then Player's 1 Score gets Reset to 0
-    document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    // Resets the Current Score back to 0, Otherwise Player's 1 Score Would Automaticly Transfer to Player 2
-    currentScore = 0;
-    // Switches the Active Player's Value from 0 to 1, which causes the Switch from Player 1 to Player 2
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    switchPlayer()
   }
 }
 
+hold.addEventListener("click", function(){
+  // savedScores[0] = savedScores[0] + currentScore;
+  savedScores[activePlayer] += currentScore;
 
+  document.querySelector(`#score--${activePlayer}`).textContent = savedScores[activePlayer];
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+
+if (savedScores[activePlayer] >= 100){
+  youWonBackground.classList.remove("play_background")
+  youWonBackground.classList.add("won_background");
+}
+
+else{
+  switchPlayer();
+}
+})
