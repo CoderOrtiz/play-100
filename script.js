158b07706ff46ba4b1eef370fa1a762e7dd1ae65
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-20 22:30:38 -0400

    Refactored All Code. More Code to Follow

--- script.js
+++ script.js
@@ -1,101 +1,113 @@
-'use strict';
-
-let score = 0;
+"use strict";
+// Set Variables
 let currentScore = 0;
 let activePlayer = 0;
-let roll = 0
+let savedScores = [0, 0];
+
+// To Select Elements within the HTML File
 let player1sTurn = document.querySelector(".player--0");
 let player2sTurn = document.querySelector(".player--1");
-let savedScores = [0, 0];
-const hideDice = () => document.querySelector("img").classList.add("hidden");
-
 const hold = document.querySelector(".btn--hold");
 const rollDice = document.querySelector(".btn--roll");
 const reset = document.querySelector(".btn--new");
 const youWonBackground = document.querySelector("body");
 
-function switchPlayer (){
-// If a Player Rolls a 1, then the Player's Score gets Reset to 0
-document.querySelector(`#current--${activePlayer}`).textContent = 0;
-// Resets the Current Score back to 0, Otherwise Player's 1 Score Would Automaticly Transfer to Player 2
-currentScore = 0;
-// Switches the Active Player's Value from 0 to 1, which causes the Switch from Player 1 to Player 2
-activePlayer = activePlayer === 0 ? 1 : 0;
-// Toggles the "player--active" Class Between Player 1 & 2
-player1sTurn.classList.toggle("player--active");
-player2sTurn.classList.toggle("player--active");
-}
-                                                      
-function scoreReset(){
-let scoreReset = document.querySelectorAll(".current-score, .score");
+// Arrow Functions Nested Within the Main Functions
+const hideDice = () => document.querySelector("img").classList.add("hidden");
+const addDiceImage = () => document.querySelector("img").classList.remove("hidden");
+const activePlayersCurrentScore = () => document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+const activePlayersSavedScore = () => document.querySelector(`#score--${activePlayer}`).textContent = savedScores[activePlayer];
+const activePlayerCurrentScoreTo_0 = () => document.querySelector(`#current--${activePlayer}`).textContent = 0;
+const allPlayersCurrentScoresTo_0 = () => document.querySelectorAll(".current-score").textContent = 0;
 
-for (let i = 0; i < scoreReset.length; i++){
-    scoreReset[i].textContent = score;
-    savedScores = [0, 0]
-}
+// A Function Declarations Nested Within the Main Functions
+function scoreReset() {
+  const scoreReset = document.querySelectorAll(".current-score, .score");
+  for (let i = 0; i < scoreReset.length; i++) {
+    scoreReset[i].textContent = 0;
+  }
 }
 
-rollDice.addEventListener("click", function(){
-  document.querySelector("img").classList.remove("hidden");
-  roll = Math.floor(Math.random() * 6) + 1;
+function switchPlayer() {
+  // If a Player Rolls a 1, then the Player's Current Score gets Reset to 0
+  activePlayerCurrentScoreTo_0();
+  // Resets the Current Score back to 0, Otherwise Player's 1 Score Would Automaticly Transfer to Player 2
+  currentScore = 0;
+  // Switches the Active Player's Value from 0 to 1, which causes the Switch from Player 1 to Player 2
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // Toggles the "player--active" Class Between Player 1 & 2
+  player1sTurn.classList.toggle("player--active");
+  player2sTurn.classList.toggle("player--active");
+}
+
+// To Hide the Dice Before Game Begins
+hideDice();
+
+
+
+// ---- Main JavaScript Code for Game ----
+
+
+
+rollDice.addEventListener("click", function () {
+  
+  addDiceImage();
+  let roll = Math.floor(Math.random() * 6) + 1;
   const diceRolled = `./images/dice-${roll}.png`;
   document.querySelector("img").setAttribute("src", diceRolled);
-  
+
   if (roll !== 1) {
     // Same as currentScore = currentScore + roll
     currentScore += roll;
-    
-   document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
-
-   
-  }else {
-    switchPlayer()
+    activePlayersCurrentScore();
+  } 
+  else {
+    switchPlayer();
   }
-})
+});
 
-hold.addEventListener("click", function(){
-  // savedScores[0] = savedScores[0] + currentScore;
+hold.addEventListener("click", function () {
+
+  // Same as savedScores[0] = savedScores[0] + currentScore;
   savedScores[activePlayer] += currentScore;
 
-  document.querySelector(`#score--${activePlayer}`).textContent = savedScores[activePlayer];
-  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayersSavedScore();
+  activePlayerCurrentScoreTo_0();
 
-if (savedScores[activePlayer] >= 100){
-  youWonBackground.classList.remove("play_background")
-  youWonBackground.classList.add("won_background");
-  document.querySelector("#score--0").textContent = savedScores[0];
-  document.querySelector("#score--1").textContent = savedScores[1];
-  document.querySelectorAll(".current-score").textContent = 0;
-  hold.disabled = true;
-  rollDice.disabled = true;
-}
+  if (savedScores[activePlayer] >= 100) {
+    youWonBackground.classList.remove("play_background");
+    youWonBackground.classList.add("won_background");
+    allPlayersCurrentScoresTo_0();
 
-else{
-  switchPlayer();
-}
-})
+    // Disables the "Roll Dice" & "Hold" Buttons once Game is Won
+    rollDice.disabled = true;
+    hold.disabled = true;
+  } 
+  else {
+    // If a Player has Reached a Score of 100 yet, then the Chance will Switch to the Other Player
+    switchPlayer();
+  }
+});
 
+reset.addEventListener("click", function () {
 
-reset.addEventListener("click", function(){
-  score = 0;
- currentScore = 0;
- activePlayer = 0;
- roll = 0
- player1sTurn = document.querySelector(".player--0");
- 
- savedScores = [0, 0];
-const hideDice = () => document.querySelector("img").classList.add("hidden");
+  // Reseting Necessary Values for the Reset Function
+  currentScore = 0;
+  activePlayer = 0;
+  savedScores = [0, 0];
 
-  document.querySelector("#current--0").textContent = currentScore;
+  //  Game will Default to Player 1 when Game is Reset
   player1sTurn.classList.add("player--active");
   player2sTurn.classList.remove("player--active");
+
+  // Removes Green Background to the Orange Background
   youWonBackground.classList.remove("won_background");
   youWonBackground.classList.add("play_background");
-  
-  hideDice()
-  scoreReset()
+
+  hideDice();
+  scoreReset();
+
+  //  Enables the "Roll Dice" & "Hold" Buttons once Game is Won
   hold.disabled = false;
   rollDice.disabled = false;
-
-  
 });
